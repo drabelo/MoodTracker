93f11b85c3dc76b032cff1bfae774f0d239a8235
drabelo <http://dailtonrabelo.com>
2015-04-30 15:14:55 -0400

    update

---
+++ app/src/main/java/beta/drab/moodtracker/AdapterModel/MoodRow.java
@@ -0,0 +1,17 @@
+package beta.drab.moodtracker.AdapterModel;
+
+/**
+ * Created by dailtonrabelo on 4/28/15.
+ */
+
+public class MoodRow {
+
+    public String name;
+
+    public MoodRow( String name ) {
+        this.name = name ;
+    }
+
+
+
+}
